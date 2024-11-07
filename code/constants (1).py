eb2f300d0eac931edb94d298155024c6b227a58c
ChidireSriHarsha <chideresriharsha@gmail.com>
2024-11-07 19:49:05 +0530

    Add files via upload

---
+++ code/constants (1).py
@@ -0,0 +1,6 @@
+# File paths
+DATASET_PATH = 'C:\Users\chide\OneDrive\Desktop\infosys>/Dataset/dynamic_pricing.csv'
+
+# Global constants for feature scaling
+FEATURES_TO_SCALE = ['Number_of_Riders', 'Number_of_Drivers', 'Expected_Ride_Duration']
+TARGET_VARIABLE = 'Historical_Cost_of_Ride'
