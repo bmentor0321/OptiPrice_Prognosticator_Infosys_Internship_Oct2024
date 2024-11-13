d61ff051f8dcec9160c60dccd78cb93f12ce527a
Shaik Noor Baji <137255603+NOORBAJI55@users.noreply.github.com>
2024-11-13 15:01:36 +0530

    Add files via upload

---
+++ Code/Model_3.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b912ebea-1d2c-4253-b34e-e1698fcb622f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
