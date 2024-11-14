1ec56e85355927bdf1e28dbf6ec39a678b253025
DeepshikhaPaty04 <deepsofficial04@gmail.com>
2024-11-14 19:35:19 +0530

    1

--- Code/Utils/constants.py
+++ Code/Utils/constants.py
@@ -1,7 +1,7 @@
 import os
 
 # Directory paths
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(_file_))))
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DATA_DIR = os.path.join(PROJECT_DIR, "Data")
 RAW_DIR = os.path.join(DATA_DIR, "Raw")
 PROCESSED_DIR = os.path.join(DATA_DIR, "Processed")
