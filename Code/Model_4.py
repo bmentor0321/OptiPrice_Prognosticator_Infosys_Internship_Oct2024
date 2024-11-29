6ebfe55b5863f1e3c88baa9ecf0a247fd56b0a5e
Virochana27 <gmvirochan@gmail.com>
2024-11-29 19:21:52 +0530

    29/11/2024

--- Code/Model_4.py
+++ Code/Model_4.py
@@ -10,41 +10,20 @@ from constants import DATASET_PATH, TARGET_VARIABLE
 # Load dataset
 data = pd.read_csv(DATASET_PATH)
 
+# Feature Engineering
+
+# Calculate Adjusted Cost
+data['Adjusted_Ride_Cost'] = np.sqrt(data['Historical_Cost_of_Ride'] * data['Expected_Ride_Duration'])
+
+data['Cost_per_Minute'] = data['Historical_Cost_of_Ride'] / data['Expected_Ride_Duration']
+
+time_cost_map = {'Morning': 1.15, 'Afternoon': 1.15, 'Evening': 1.1, 'Night': 1.2}
+data['Booking_Time_Cost_Impact'] = data['Time_of_Booking'].map(time_cost_map) * data['Historical_Cost_of_Ride']
+
 # Convert `Average_Ratings` to categorical by binning
 rating_bins = [0, 4.0, 4.5, 5.0]  # Define bin ranges
 rating_labels = ['Low', 'Average', 'High']  # Define labels for each bin
 data['Average_Ratings_Cat'] = pd.cut(data['Average_Ratings'], bins=rating_bins, labels=rating_labels)
-
-# Feature Engineering
-
-#1-63.61
-# Calculate Adjusted Cost
-data['Adjusted_Ride_Cost'] = np.sqrt(data['Historical_Cost_of_Ride'] * data['Expected_Ride_Duration'])
-
-#2-17.69
-data['Cost_per_Minute'] = data['Historical_Cost_of_Ride'] / data['Expected_Ride_Duration']
-
-#3-58.88
-time_cost_map = {'Morning': 1.15, 'Afternoon': 1.15, 'Evening': 1.1, 'Night': 1.2}
-data['Booking_Time_Cost_Impact'] = data['Time_of_Booking'].map(time_cost_map) * data['Historical_Cost_of_Ride']
-
-#4-54.47
-#loyalty_cost_map = {'Silver': 0.9, 'Regular': 1.0, 'Gold': 0.85}
-#data['Loyalty_Cost_Impact'] = data['Customer_Loyalty_Status'].map(loyalty_cost_map) * data['Historical_Cost_of_Ride']
-
-#5-46.18
-#location_cost_map = {'Urban': 1.2, 'Suburban': 1.1, 'Rural': 0.9}
-#data['Location_Cost_Multiplier'] = data['Location_Category'].map(location_cost_map)
-#data['Location_Adjusted_Cost'] = data['Historical_Cost_of_Ride'] * data['Location_Cost_Multiplier']
-
-#6-7.34
-#data['Cost_per_Rider'] = data['Historical_Cost_of_Ride'] / data['Number_of_Riders']
-
-#7-25.8
-#data['Duration_Adjusted_Cost'] = data['Historical_Cost_of_Ride'] * (data['Expected_Ride_Duration'] / data['Expected_Ride_Duration'].mean())
-
-#8-11.36
-#data['Loyalty_Cost_Impact'] = data['Number_of_Past_Rides'] * data['Historical_Cost_of_Ride']
 
 # One-hot encode categorical features
 data = pd.get_dummies(data, columns=['Location_Category', 'Customer_Loyalty_Status', 'Time_of_Booking', 'Vehicle_Type', 'Average_Ratings_Cat'], drop_first=True)
@@ -52,8 +31,6 @@ data = pd.get_dummies(data, columns=['Location_Category', 'Customer_Loyalty_Stat
 # Separate independent variables (X) and target variable (y)
 X = data.drop(columns=[TARGET_VARIABLE])
 y = data[TARGET_VARIABLE]
-
-
 
 # Split the data into training, validation, and test sets
 X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
--- Code/constants.py
+++ Code/constants.py
@@ -1,6 +1,6 @@
 # File paths
 DATASET_PATH = 'C:/Users/viroc/Documents/Infosys Springboard Internship/Project/OptiPrice_Prognosticator_Infosys_Internship_Oct2024/Dataset/dynamic_pricing.csv'
-
+REGRESSION_ANALYSIS_DATASET_PATH = 'C:/Users/viroc/Documents/Infosys Springboard Internship/Project/OptiPrice_Prognosticator_Infosys_Internship_Oct2024/Dataset/Regression_Analysis_Dataset.csv'
 # Global constants for feature scaling
 FEATURES_TO_SCALE = ['Number_of_Riders', 'Number_of_Drivers', 'Expected_Ride_Duration']
 TARGET_VARIABLE = 'Historical_Cost_of_Ride'
